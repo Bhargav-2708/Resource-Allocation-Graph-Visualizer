aa18b23a540971f751e9e9edd146de06dd92d289
K.V.Bhargav <Bhargav-2708@users.noreply.github.com>
2026-04-25 11:57:43 +0530

    Fix: use relative base path for GH Pages compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Resource-Allocation-Graph-Visualizer/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
